fd4616ec5fd5f9718efc0c3b72c641ea4f594329
joshua Hernandez <joshua.hdz.reza@gmail.com>
2021-03-01 07:23:58 +0000

    walkthrough: project-1-fetch

---
+++ portfolio/src/main/webapp/servedDate.js
@@ -0,0 +1,10 @@
+/*
+    Adds current date to index.html
+*/
+async function showDate() {
+  const responseFromServer = await fetch('/date');
+  const textFromResponse = await responseFromServer.text();
+
+  const dateContainer = document.getElementById('date-container');
+  dateContainer.innerText = textFromResponse;
+}
