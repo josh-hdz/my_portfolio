3bf884fd9f9e721cc608f62fd3c0953900d5f3e6
joshua Hernandez <joshua.hdz.reza@gmail.com>
2021-03-01 19:42:54 +0000

    refactor: data format improve, more readable.

--- portfolio/src/main/java/com/google/sps/servlets/ServedDate.java
+++ portfolio/src/main/java/com/google/sps/servlets/ServedDate.java
@@ -1,11 +1,15 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 
 /** Servlet that responds with the current date. */
 @WebServlet("/date")
@@ -13,7 +17,10 @@ public class ServedDate extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Date date = Calendar.getInstance().getTime();
+    DateFormat formatter = new SimpleDateFormat("EEE, dd MMM yyyy");
+
     response.setContentType("text/html;");
-    response.getWriter().println("" + new Date());
+    response.getWriter().println(formatter.format(date));
   }
 }
