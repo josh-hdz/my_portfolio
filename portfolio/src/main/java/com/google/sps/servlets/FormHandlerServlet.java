5ca60a4a363c43cfaeabed5aeb5f1aaed680725c
joshua Hernandez <joshua.hdz.reza@gmail.com>
2021-03-08 20:23:44 +0000

    feature: data is now stored instead of print in console

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,10 +1,17 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
@@ -12,14 +19,26 @@ public class FormHandlerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String reason = request.getParameter("reason");
-    String description = request.getParameter("description");
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("FORM RECIVED");
-    System.out.println(reason + ": " + description);
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Comment");
+    FullEntity taskEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set(
+                "reason",
+                Jsoup.clean(request.getParameter("reason"),Whitelist.none())
+            )
+            .set(
+                "description",
+                Jsoup.clean(request.getParameter("description"),Whitelist.none())
+            )
+            .set(
+                "timestamp",
+                System.currentTimeMillis()
+            )
+            .build();
+    datastore.put(taskEntity);
 
-    response.sendRedirect("https://jhernandez-sps-spring21.appspot.com");
+    response.sendRedirect("/index.html");
   }
 }
