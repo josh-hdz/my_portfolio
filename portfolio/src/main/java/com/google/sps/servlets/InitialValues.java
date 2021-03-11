c0625fcfcf84ebcfa705c5ad625c2436202537a1
joshua Hernandez <joshua.hdz.reza@gmail.com>
2021-03-11 19:06:30 +0000

    rename file. It now handles all initial values needed for website to propertly load

--- portfolio/src/main/java/com/google/sps/servlets/InitialValues.java
+++ portfolio/src/main/java/com/google/sps/servlets/InitialValues.java
@@ -1,6 +1,7 @@
 package com.google.sps.servlets;
 
 import com.google.gson.Gson;
+import com.google.sps.data.GoogleMap;
 import com.google.sps.data.HeaderInfo;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -10,15 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 
 
 /** Servlet handles info needed for header (greeting & date) */
-@WebServlet("/header-handler")
-public class HeaderHandler extends HttpServlet {
+@WebServlet("/initial-values")
+public class InitialValues extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Gson converter = new Gson();
-        HeaderInfo headerInfo = new HeaderInfo(5);
-        String json = converter.toJson(headerInfo);
+        String headerInfo = converter.toJson(new HeaderInfo(5));
+        String mapInfo = converter.toJson(new GoogleMap());
+
         response.setContentType("application/json;");
-        response.getWriter().println(json);
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().println('['+headerInfo+','+mapInfo+']');
     }
 }
